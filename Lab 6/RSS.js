d60bc59d7147ce8f53d9da7c75ca1a15782f3d7d
ChronicleWolfVR <ravivarunpravin@gmail.com>
2023-10-11 18:31:08 +0100

    Added Dom Parser moddule

--- Lab 6/RSS.js
+++ Lab 6/RSS.js
@@ -1,11 +1,21 @@
+const { DOMParser } = require('xmldom');
 
 const getRssFeed = async ()=> {
-    let prom = await fetch( 'https://moxie.foxnews.com/google-publisher/latest.xml' );  
-let text = await prom.text();
-console.log( text.substring(0, 100 ) );
+    let prom = await fetch( 'https://moxie.foxnews.com/google-publisher/latest.xml' );
+    let text = await prom.text();
+    
+    console.log( text.substring(0, 100 ) );
+    
+    let parser = new DOMParser();
+    let xmlDoc = parser.parseFromString(text, "text/xml");
+    
+    console.log( xmlDoc );
+    
+    const items = xmlDoc.querySelectorAll("item");
+    console.log('items:', items );
 
 }
 
 getRssFeed();
 
-console.log('end of script');
+console.log( 'end')
