cf0909cb20595fa54052382f2eb261e91a076315
ChronicleWolfVR <ravivarunpravin@gmail.com>
2023-10-11 18:34:04 +0100

    Added error handaling

--- Lab 6/RSS.js
+++ Lab 6/RSS.js
@@ -1,19 +1,26 @@
 const { DOMParser } = require('xmldom');
 
 const getRssFeed = async ()=> {
-    let prom = await fetch( 'https://moxie.foxnews.com/google-publisher/latest.xml' );
-    let text = await prom.text();
-    
-    console.log( text.substring(0, 100 ) );
-    
-    let parser = new DOMParser();
-    let xmlDoc = parser.parseFromString(text, "text/xml");
-    
-    console.log( xmlDoc );
-    
-    const items = xmlDoc.querySelectorAll("item");
-    console.log('items:', items );
+    try {
+        let prom = await fetch( 'https://moxie.foxnews.com/google-publisher/latest.xml' );
+        let text = await prom.text();
 
+        console.log( text.substring(0, 100 ) );
+
+        let parser = new DOMParser();
+        let xmlDoc = parser.parseFromString(text, "text/xml");
+
+        console.log( xmlDoc );
+
+        if (xmlDoc.documentElement.nodeName == "parsererror") {
+            throw new Error("XML parsing error");
+        }
+
+        const items = xmlDoc.getElementsByTagName("item");
+        console.log('items:', items );
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 getRssFeed();
