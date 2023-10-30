f4fe35e82840a1fdf867e486a340d4a69ec51e34
ChronicleWolfVR <ravivarunpravin@gmail.com>
2023-10-30 13:06:22 +0000

    Node server to client set up

--- Lab 7/nodePlayground/index.js
+++ Lab 7/nodePlayground/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const {readFile, read} = require('fs');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    readFile('./home.html', 'utf8', (err, html) => {
+        if(err){
+            res.status(500).send('Sorry, out of order');
+        }
+        res.send(html);
+    });
 }   );
+
+app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
