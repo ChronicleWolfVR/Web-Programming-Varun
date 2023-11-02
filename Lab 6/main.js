41a057856a934168a1ca22c9adca61cb05a48769
ChronicleWolfVR <ravivarunpravin@gmail.com>
2023-11-02 09:31:05 +0000

    Added new server file

---
+++ Lab 6/main.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {readFile, read} = require('fs'); //require modules
+
+const app = express(); //create express app
+
+app.get('/', (req, res) => { //create route
+    readFile('./index.html', 'utf8', (err, html) => { //read file
+        if(err){
+            res.status(500).send('Sorry, out of order');   //error handling
+        }
+        res.send(html); //send response
+    });
+}   );
+
+app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
